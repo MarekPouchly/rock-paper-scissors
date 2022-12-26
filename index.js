e348ad6e8ed28cd05c3ad232c97be53774b7c86b
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-26 14:07:50 +0100

    Single Round function with case insensitive player selection

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
     return symbols[randomNumber]
 }
+
+const playerSelection = "RocK"
+const computerSelection = getComputerChoice()
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    console.log(playerSelection)
+    console.log(computerSelection)
+
+    if (playerSelection == computerSelection) {
+        return "Tie"
+    } 
+    else if (playerSelection == "Rock") {
+        return (computerSelection == "Paper") ? "You lose" : "You won";
+    } 
+    else if (playerSelection == "Paper") {
+        return (computerSelection == "Scissors") ? "You lose" : "You won";
+    } 
+    else if (playerSelection == "Scissors") {
+        return (computerSelection == "Rock") ? "You lose" : "You won";
+    }
+}
