960e9a513e5db5452ef13335307197f6b515495a
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-26 14:39:53 +0100

    Play function

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
+let playerSelection = ""
+let computerSelection = ""
+
 function getComputerChoice() {
     const symbols = ["Rock", "Paper", "Scissors"]
     let randomNumber = Math.floor(Math.random() * 3)
     return symbols[randomNumber]
 }
 
-const playerSelection = "RocK"
-const computerSelection = getComputerChoice()
-
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-    console.log(playerSelection)
-    console.log(computerSelection)
-
     if (playerSelection == computerSelection) {
-        return "Tie"
+        return 2
     } 
     else if (playerSelection == "Rock") {
-        return (computerSelection == "Paper") ? "You lose" : "You won";
+        return (computerSelection == "Paper") ? 0 : 1;
     } 
     else if (playerSelection == "Paper") {
-        return (computerSelection == "Scissors") ? "You lose" : "You won";
+        return (computerSelection == "Scissors") ? 0 : 1;
     } 
     else if (playerSelection == "Scissors") {
-        return (computerSelection == "Rock") ? "You lose" : "You won";
+        return (computerSelection == "Rock") ? 0 : 1;
+    }
+}
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    let tie = 0
+
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice()
+        playerSelection = prompt("Select Rock, Paper or Scissors")
+
+        if (playRound(playerSelection, computerSelection) === 0) {
+            computerScore += 1;
+        } else if (playRound(playerSelection, computerSelection) === 1) {
+            playerScore += 1;
+        } else if (playRound(playerSelection, computerSelection) === 2) {
+            tie += 1;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        alert(`You are a winner: ${playerScore}:${computerScore}`)
+    } else if (playerScore < computerScore) {
+        alert(`You lost: ${playerScore}:${computerScore}`)
+    } else {
+        alert(`Tie: ${playerScore}:${computerScore}`)
     }
 }
